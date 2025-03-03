24e5b1ff7afd50f5cc5c29b6788910ed31129d17
Mariam Zenhom <zenhommariam140@gmail.com>
2025-03-03 23:04:39 +0200

    add login api

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,20 @@
 <?php
 
+use App\Http\Controllers\API\UserController;
 use Laravel\Socialite\Facades\Socialite;
 use App\Http\Controllers\API\GoogleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::get('/auth/google', [GoogleController::class, 'redirectToGoogle']);
-Route::get('/auth/google-callback', [GoogleController::class, 'handleGoogleCallback']);
+Route::controller(GoogleController::class)->group(function () {
+    Route::get('/auth/google',  'redirectToGoogle');
+    Route::get('/auth/google/callback',  'handleGoogleCallback');
+});
+
+Route::post('/auth/register', [UserController::class,'createUser']);
+Route::post('/auth/login', [UserController::class,'loginUser']);
 
