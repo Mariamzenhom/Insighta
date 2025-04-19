84b54d327ca00bd80c062c62f5428a49aec8f541
Mariam Zenhom <zenhommariam140@gmail.com>
2025-04-19 11:42:20 +0200

    add FacebookController

---
+++ app/Http/Controllers/API/FacebookController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
+
+class FacebookController extends Controller
+{
+    use HasApiTokens;
+    public function redirectToFacebook()
+    {
+
+        return response()->json([
+            'url' => Socialite::driver('facebook')->stateless()->redirect()->getTargetUrl()
+        ]);
+    }
+
+    public function handleFacebookCallback()
+    {
+
+        try {
+            $facebookUser = Socialite::driver('facebook')->stateless()->user();
+
+            $user = User::where('social_id', $facebookUser->id)->first();
+
+            if ($user) {
+                // Update user details
+                $user->facebook_token = $facebookUser->facebook_token;
+
+            } else {
+                // Create a new user
+                $userData = User::create([
+                    'name' => $facebookUser->name,
+                    'email' => $facebookUser->email,
+                    'social_id' => $facebookUser->id,
+                    'social_type' => 'facebook',
+                    'password' => Hash::make('random_password')
+                ]);
+            }
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User authenticated successfully',
+                'user' => $user,
+                'token' => $token
+            ]);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Authentication failed',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+}
