57667709967602dea3f72657ce4cace2ef5b0fba
Mariam Zenhom <zenhommariam140@gmail.com>
2025-05-11 13:07:58 +0300

    حفظ تعديلاتي قبل الدمج

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\API\FacebookController;
 use App\Http\Controllers\API\GoogleController;
-use App\Http\Controllers\UsageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -31,11 +30,6 @@ Route::post('/auth/logout', [AuthenticatedSessionController::class, 'destroy'])
     ->middleware('auth:sanctum');
 
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/usage/start', [UsageController::class, 'startSession']);
-    Route::post('/usage/end', [UsageController::class, 'endSession']);
-    Route::get('/usage/report', [UsageController::class, 'getReport']);
-    Route::post('/usage/check-threshold', [UsageController::class, 'checkThreshold']);
-});
+
 
 
