dd201d52332cefb6b39c489dd0e24d4a07a10203
Your Name <you@example.com>
2025-06-03 16:17:15 +0000

    fix parentcontroller route

--- modules/User/Resources/routes/api.php
+++ modules/User/Resources/routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\User\Controllers\UserController;
+use Modules\User\Controllers\PerantController;
 
 
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::post('/parent/select-child', [UserController::class, 'selectChildAndSendOtp'])->name('parent.selectChildAndSendOtp');
+    Route::post('/parent/select-child', [PerantController::class, 'selectChildAndSendOtp'])->name('parent.selectChildAndSendOtp');
 
-    Route::post('/child/verify-otp', [UserController::class, 'verifyOtp'])->name('child.verifyOtp');
+    Route::post('/child/verify-otp', [PerantController::class, 'verifyOtp'])->name('child.verifyOtp');
 });
